4c5e0c906bc47994478b657a83444078d5458019
kckaiwei <kckaiwei@gmail.com>
2016-03-08 12:44:18 -0600

    time change

--- twitterpostbot.py
+++ twitterpostbot.py
@@ -52,7 +52,7 @@ def search(twts):
             api.create_favorite(i.id)
             print "JUST FAVORITED " + (i.text)
         # This part waits a minute before moving onto the next one.
-        time.sleep(480)
+        time.sleep(300)
 
 
 def run():
